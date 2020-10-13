d25a2fc62dae3e5005aaef531c39066b5612522a
robcrusoe <arka.sain@aol.com>
2020-10-13 22:34:25 +0530

    Implements examples for using the request object

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const Koa = require('koa');
 const app = new Koa();
 
+
+/* Add a date method to the 'context' */
+app.context.date = Date();
+
+
+/* Response */
 app.use(ctx => {
-    ctx.body = 'Hello, There!';
+    // Uses the state
+    ctx.state.user = 'John Doe';
+
+    // Uses the request object
+    let from = ctx.request.origin;
+    let method = ctx.request.method;
+
+    // Prints out message with date
+    ctx.body = `Hello, There! ${ctx.state.user} on ${ctx.date}`;
+    console.log(from, method);
 });
 
 app.listen(3000);
